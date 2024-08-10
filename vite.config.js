05ecce808924e2c97aacb680d78f37e8ffd93f3f
jesus bloise <jesusbloise@gmail.com>
2024-08-10 16:17:22 -0400

    agregando las dependencia he instalaciones faltantes para poder correr

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-multi-carousel', 'react-icons/fa', 'react-slick'],// Asegúrate de incluirlo aquí si es necesario
+    },
+  },
 });
